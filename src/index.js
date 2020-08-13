f2ddee0276d6395e286aa153a3931ef45ad90ba8
Brandan Sayarath <brandan@brandan.tech>
2020-08-13 11:43:03 -0700

    added border and removed conditional rendering from render method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import "./SeasonDisplay.css";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -13,16 +14,24 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner message="Please accept location request"/>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border-red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
